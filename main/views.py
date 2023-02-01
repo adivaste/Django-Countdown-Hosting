a89e4d090995b68984b64ffefd683bbeda624cd9
AV <92280537+adivaste@users.noreply.github.com>
2023-02-01 17:18:06 +0000

    Switched DB frm JSON to Sqlite3

--- main/views.py
+++ main/views.py
@@ -1,14 +1,13 @@
 """This module contains the views functions """
 
 import json
-import os
 import secrets
 import math
 from datetime import datetime
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-
+from main import models
 
 def generate_random_string(length=3):
     """This will generate the Random String for URL"""
@@ -22,19 +21,10 @@ def index(request):
     if request.method == 'POST':
         data = json.loads(request.body)
         uri = generate_random_string()
-        data_to_write = [uri, data['timestamp'], data['title']]
 
-        if not os.path.exists('data.json'):
-            with open('data.json', 'w', encoding="UTF-8") as file:
-                json.dump({}, file)
-
-        with open("data.json", 'r+', encoding="UTF-8") as file:
-            json_data = json.load(file)
-            json_data[uri] = data_to_write
-
-            file.seek(0)
-            json.dump(json_data, file)
-            file.truncate()
+        countdown_info = models.CountdownInfo(title=data['title'], timestamp=data['timestamp'],
+        uri=uri)
+        countdown_info.save()
 
         return JsonResponse({"uri": uri})
 
@@ -42,29 +32,27 @@ def index(request):
     return response
 
 def countdown(request,time):
-    """This function will render a countdown template with countdown"""
+    """This function will render a countdown template with countdown [endpoint : /countdown/56]"""
     context = { "time" : time }
     response = render(request,'main/countdownexpire.html', context)
     return response
 
 def timer(request,uri):
-    """Set timer function and store to database"""
+    """Set timer function and store to database [endpoint : /timer/rQWAsa]"""
     status = False
     timestamp_title = 'Title'
     existing_timestamp_in_sec = 0
     current_timestamp_in_sec = math.floor(datetime.now().timestamp())
 
-    with open('data.json','r', encoding="UTF-8") as file:
-        try :
-            data = json.load(file)
-            existing_timestamp_in_sec = data[uri][1]
-            timestamp_title = data[uri][2]
-            status = True
-        except :
-            return render(request, 'main/error.html')
+    countdown_info =  models.CountdownInfo.objects.get(uri=uri)
+    if countdown_info:
+        existing_timestamp_in_sec = int(countdown_info.timestamp)
+        timestamp_title = countdown_info.title
+        status = True
 
     time_response =  existing_timestamp_in_sec - current_timestamp_in_sec
     context = { "time" : 0, 'title': timestamp_title, 'status': status }
+
     if time_response > 0 :
         context['time'] = time_response
     response = render(request,'main/countdownexpire.html', context)
